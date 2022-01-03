ff957a18b2b10849e18057801000c3b8a31233b1
codingxjourney <codingxjourney@gmail.com>
2022-01-03 09:41:09 +0530

    chapter 1 created FuzzyEq trait and own assert_fuzzy_eq! macro

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,42 @@
+pub trait FuzzyEq<T> {
+    fn fuzzy_eq(&self, other: &T) -> bool;
+
+    fn fuzzy_ne(&self, other: &T) -> bool {
+        !self.fuzzy_eq(other)
+    }
+}
+
 pub fn f64_fuzzy_eq(left: f64, right: f64) -> bool {
     let epsilon = 0.00001;
     (left - right).abs() < epsilon
 }
+
+
+// Not really sure what I am doing here, as I don't have a great understanding
+// of macros yet! But I will fix and understand in future.
+
+#[macro_export]
+macro_rules! assert_fuzzy_eq {
+    ($left:expr, $right:expr $(,)?) => ({
+        match (&$left, &$right) {
+            (left_val, right_val) => {
+                if left_val.fuzzy_ne(right_val) {
+                    panic!("asserting, fuzzy equality, {:?} is not fuzzy equal to {:?}", left_val, right_val);
+                }
+            }
+        }
+    });
+}
+    // ($left:expr, $right:expr, $($arg:tt)+) => ({
+    //     match (&$left, &$right) {
+    //         (left_val, right_val) => {
+    //             if !(*left_val == *right_val) {
+    //                 let kind = $crate::panicking::AssertKind::Eq;
+    //                 // The reborrows below are intentional. Without them, the stack slot for the
+    //                 // borrow is initialized even before the values are compared, leading to a
+    //                 // noticeable slow down.
+    //                 $crate::panicking::assert_failed(kind, &*left_val, &*right_val, $crate::option::Option::Some($crate::format_args!($($arg)+)));
+    //             }
+    //         }
+    //     }
+    // });
